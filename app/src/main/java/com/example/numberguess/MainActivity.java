6181b3c21abe489fca2d44a1e3fd61709f5f5dde
natseeto <61493185+natseeto@users.noreply.github.com>
2020-10-06 07:37:31 +1100

    A lot of shit

--- app/src/main/java/com/example/numberguess/MainActivity.java
+++ app/src/main/java/com/example/numberguess/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.numberguess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView desc;
     private TextView higher;
     private TextView lower;
-    private EditText userGuess;
+    private TextView number;
+    private EditText userInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +24,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Number Guess Game");
-        int max = 100;
-        int min = 0;
-        double x = (Math.random()*((max-min)+1))+min;
-
+        
         desc = findViewById(R.id.desc);
         desc.setText("You have 5 trials to guess a random generated number between 0 and 100. You have 5 attempts and will be told if the number is higher or lower");
 
         guessButton = findViewById(R.id.guessButton);
-        guessButton.setText(String.valueOf(x));
-        userGuess = findViewById(R.id.userGuess);
+        higher = findViewById(R.id.higher);
+        lower = findViewById(R.id.lower);
+        number = findViewById(R.id.number);
+        userInput = findViewById(R.id.userInput);
 
+
+        guessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int max = 100;
+                int min = 0;
+                double x = Math.round((Math.random() * ((max - min) + 1)) + min);
+                    int guess = Integer.parseInt(userInput.getText().toString());
+                    if (checkGuess((int) x, guess)) {
+                        guessButton.setText("Good shit");
+                    } else {
+                        guessButton.setText("try again bitch");
+                    }
+            }
+        });
+    }
+
+    private boolean checkGuess(int number, int guess) {
+        if (number == guess) {
+            Intent intent = new Intent(this, Result.class);
+            startActivity(intent);
+            higher.setVisibility(View.INVISIBLE);
+            lower.setVisibility(View.INVISIBLE);
+            return true;
+        } else if (number < guess) {
+            higher.setVisibility(View.VISIBLE);
+            lower.setVisibility(View.INVISIBLE);
+        } else {
+            higher.setVisibility(View.INVISIBLE);
+            lower.setVisibility(View.VISIBLE);
+            return false;
+        } return false;
     }
 }
